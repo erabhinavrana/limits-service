f04514c80894e8f2ebd11710d4db7bc6d59fae7b
Abhinav <erabhinav.rana@gmail.com>
2019-04-23 10:11:58 +0400

    Configuring fault tolerance using netflix hystrix.

--- src/main/java/com/abhi/microservices/limitsservice/controller/LimitsConfigController.java
+++ src/main/java/com/abhi/microservices/limitsservice/controller/LimitsConfigController.java
@@ -2,6 +2,7 @@ package com.abhi.microservices.limitsservice.controller;
 
 import com.abhi.microservices.limitsservice.beans.LimitConfiguration;
 import com.abhi.microservices.limitsservice.configuration.LimitPropertiesConfig;
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -19,4 +20,16 @@ public class LimitsConfigController {
     public LimitConfiguration getLimitsFromConfig(){
         return new LimitConfiguration(limitPropertiesConfig.getMaximum(), limitPropertiesConfig.getMinimum());
     }
+
+    @GetMapping("/hystrix-example")
+    @HystrixCommand(fallbackMethod = "getLimitsFromConfigFallbackMethod")
+    public LimitConfiguration faultToleranceUsingHystrix(){
+        throw new RuntimeException("Service not available!!"); // Logic that may cause the runtime exception due to the failure of calling external service
+    }
+
+    public LimitConfiguration getLimitsFromConfigFallbackMethod(){
+        return new LimitConfiguration(1000, 10); // default configuration in case of failure of the service
+    }
+
+
 }
